410dcdfae58fa574ca29a81403bcbf8dceb44b29
Alexandra-Myers <bia416cat@gmail.com>
2022-07-03 21:53:33 -0400

    initial comment

---
+++ src/main/java/net/atlaspatchteam/atlaspatcherapi/AtlasPatcherMain.java
@@ -0,0 +1,19 @@
+package net.atlaspatchteam.atlaspatcherapi;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AtlasPatcherMain implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod name as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final String MOD_ID = "atlas";
+	public static final Logger LOGGER = LoggerFactory.getLogger("Atlas API");
+
+	@Override
+	public void onInitialize(ModContainer mod) {
+		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
+	}
+}
